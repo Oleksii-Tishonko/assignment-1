e6570102d8dd27814756850f21ad9933fb2ef74b
Oleksii Tishonko <OleksiiTishonko@gmail.com>
2023-11-22 11:16:02 -0700

    add Start and Stop buttons

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import './App.css'
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [running, setRunning] = useState(true);
 
   useEffect(() => {
     console.log('useEffect runs');
-    const timerId = setInterval(() =>{
-      setCounter(counter + 1);
-    }, 3000);
+
+    let timerId = null;
+    if(running){
+      timerId = setInterval(() =>{
+        setCounter(counter + 1);
+      }, 3000);
+    }
 
     return () => {clearInterval(timerId)};
-  }, [counter]);
+  }, [counter, running]);
 
   return (
     <>
     <div className='counter'>{counter}</div>
     <div className='actionPanel'>
-      <button onClick={() => setCounter(0)}>reset</button>
+      <button onClick={() => setCounter(0)}>Reset</button>
+      <button onClick={() => setRunning(true)}>Start</button>
+      <button onClick={() => setRunning(false)}>Stop</button>
     </div>
     </>
   )
